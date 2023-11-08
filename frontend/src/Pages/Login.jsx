e5684aae65dd7141e0fcbecffa017884b3f2741e
Reniel George Mababa <mabsrencode@gmail.com>
2023-11-08 12:00:36 +0800

    i

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials)
+      const res = await axios.post("q-zone-api.onrender.com/auth/login", credentials)
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
       navigate("/")
     } catch (err) {
--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -36,7 +36,7 @@ const Registration = () => {
     dispatch({ type: 'REGISTER_START' });
 
     try {
-      const res = await axios.post('/auth/register', credentials);
+      const res = await axios.post('q-zone-api.onrender.com/auth/register', credentials);
       dispatch({ type: 'REGISTER_SUCCESS', payload: res.data });
       navigate('/'); //! new update
     } catch (err) {
