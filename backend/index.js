53127fd331c18fe3ac9d9ef9deda32fa7c907644
Reniel George Mababa <mabsrencode@gmail.com>
2024-07-22 00:21:08 +0800

    node cron

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 // const axios = require("axios");
+const cron = require("node-cron");
+const http = require("http");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,5 +20,14 @@ app.use("/api", servicesRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/bookings", bookingRoute);
 app.use("/api/contact", contactRoute);
-
+const pingServer = () => {
+  http
+    .get("https://segen-consulting.onrender.com", (res) => {
+      console.log("Pinged server, status code:", res.statusCode);
+    })
+    .on("error", (err) => {
+      console.error("Error pinging server:", err.message);
+    });
+};
 app.listen(port, () => console.log(`Starting server on port ${port}`));
+cron.schedule("*/5 * * * *", pingServer);
